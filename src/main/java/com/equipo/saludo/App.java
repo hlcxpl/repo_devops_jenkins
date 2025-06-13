e112b118cb02bcdb7db9360e3634e567a73c04e6
hlcxpl <hlcxpl@gmai.com>
2025-06-12 23:14:16 -0400

    Proyecto Java Maven SaludoApp

---
+++ src/main/java/com/equipo/saludo/App.java
@@ -0,0 +1,15 @@
+package com.equipo.saludo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static String saludar(String nombre) {
+        return "¡Hola, " + nombre + "!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(saludar("DevOps"));
+    }
+}
